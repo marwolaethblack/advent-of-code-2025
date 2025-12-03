dc9d7cc61f94a15014e9cb20041daf51bd8f3235
Adnan Unal <a.unal677@gmail.com>
2025-12-03 12:19:46 +0100

    feat: day 2 part 1

---
+++ lib/day02/solution.ex
@@ -0,0 +1,59 @@
+defmodule AOC.Day02 do
+  require Integer
+
+  def parse_input(filename) do
+    case File.read(filename) do
+      {:ok, contents} ->
+        String.trim(contents) |> String.split(",")
+
+      {:error, :enoent} ->
+        []
+    end
+  end
+
+  def parse_range(range) do
+    String.split(range, "-") |> Enum.map(fn x -> String.to_integer(x) end)
+  end
+
+  def part1() do
+    ranges = parse_input("input.txt")
+    parsed_ranges = ranges |> Enum.map(fn range -> parse_range(range) end)
+
+    invalid_ids =
+      parsed_ranges
+      |> Enum.reduce([], fn range, acc ->
+        [st, ed] = range
+        range_list = Enum.to_list(st..ed)
+
+        invalid_ids =
+          range_list
+          |> Enum.filter(fn number ->
+            integer_string = Integer.to_string(number)
+            string_length = integer_string |> String.length()
+            is_even = string_length |> Integer.is_even()
+
+            case is_even do
+              true ->
+                {part1, part2} = integer_string |> String.split_at(div(string_length, 2))
+                part1 == part2
+
+              false ->
+                false
+            end
+          end)
+
+        acc = acc ++ invalid_ids
+        acc
+      end)
+
+    sum = Enum.sum(invalid_ids)
+    IO.inspect("Result: #{sum}")
+
+    sum
+  end
+
+  def part2() do
+  end
+end
+
+AOC.Day02.part1()
